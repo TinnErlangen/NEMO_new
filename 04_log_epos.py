1f5e8608f378a6f90d1f0c8da5629123ded280ac
kimcoco <41998428+kimcoco@users.noreply.github.com>
2020-08-25 16:40:55 +0200

    log fin

--- 04_log_epos.py
+++ 04_log_epos.py
@@ -21,7 +21,7 @@ subjs = ["NEM_10","NEM_11","NEM_12","NEM_14","NEM_15",
          "NEM_29","NEM_31","NEM_34","NEM_35","NEM_36"]
 # subjs = ["NEM_10"]
 runs = ["1","2","3","4"]
-# runs = ["4"]
+# runs = ["3","4"]
 # create new lists, if you want to try things on single subjects or runs
 
 #collecting the files for annotation
@@ -40,7 +40,7 @@ class Cycler():
         self.fn = self.filelist.pop(0)
         self.epo = mne.read_epochs(self.fn)
         self.epo.plot(n_epochs=10,n_channels=90,scalings=dict(mag=3e-12)) #these parameters work well for inspection of my 2sec epochs
-        self.fig = self.epo.plot_psd(fmax=95,average=False)
+        self.fig = self.epo.plot_psd(fmax=95,bandwidth=1,average=False)
 
     def plot(self,n_epochs=10,n_channels=90):
         self.epo.plot(n_epochs=n_epochs,n_channels=n_channels,scalings=dict(mag=3e-12))
@@ -57,14 +57,33 @@ class Cycler():
             file.write("\n\n")
             file.write(str(self.epo.drop_log))
             file.write("\n\n")
-            drops = self.epo.drop_log.count(('BAD_',))
-            inds = []
+            drops_b = self.epo.drop_log.count(('BAD_',))
+            inds_b = []
             start = 0
-            for d in range(drops):
+            for d in range(drops_b):
                 i = self.epo.drop_log.index(('BAD_',), start)
-                inds.append(i)
+                inds_b.append(i)
                 start = i + 1
-            file.write("Bad epochs: "+str(drops)+" Original index numbers: "+str(inds))
+            file.write("Bad epochs: "+str(drops_b)+" Original index numbers: "+str(inds_b))
+            drops_u = self.epo.drop_log.count(('USER',))
+            inds_u = []
+            start = 0
+            for d in range(drops_u):
+                i = self.epo.drop_log.index(('USER',), start)
+                inds_u.append(i)
+                start = i + 1
+            file.write("\nUser dropped epochs: "+str(drops_u)+" Original index numbers: "+str(inds_u))
+            if self.fn[-9] in ['3','4']:
+                inds = inds_b + inds_u
+                tris = []
+                for ind in inds:
+                    tri = ind / 5
+                    if ind % 5 != 0:
+                        tri = tri + 1
+                    tris.append(int(tri))
+                if self.fn[-9] == '4':
+                    tris = [t+32 for t in tris]
+                file.write("\nAffected trial numbers: "+str(tris))
 
 cyc = Cycler(filelist)
 
